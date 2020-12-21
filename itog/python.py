d3084d4a2dc90365252ffc020a36f776129d5537
sophiabelkova <71042084+sophiabelkova@users.noreply.github.com>
2020-12-21 21:05:03 +0300

    Add files via upload

---
+++ itog/python.py
@@ -0,0 +1,250 @@
+#класс, определение и вызов функций генерации тегов и последующего объединения в готовый файл.
+#name-название тега,  border-cкобки, позволяющие добавлять другие теги внутрь главного, content-то что будет написано, href-ссылки, классы и т д
+#функция generate пока тэг в границах тега выводим res повторяем функцию
+#функция генерации файла открывает файл page только для записи в него функции генерации тегов и закрывает файл.
+class Tag:
+    def __init__(self):
+        self.name = "name"
+        self.content = ""
+        self.href = ""
+        self.type = ""
+        self.border = []
+        self.rel = ""
+       
+        
+    def Generate(self):
+        res = "<"+self.name+" "+self.href+">"+self.content+" \n"
+        for tag in self.border:
+            res = res+tag.Generate()
+        res = res+"</"+self.name+"> \n"
+        return res
+    def GenerateFile(self, file):
+        file = open(file, "w")
+        file.write(self.Generate())
+        file.close()
+    pass
+#экземпляры класса использующие только атрибут дата
+#1 <html>
+class Html(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "html"
+        self.border = data
+    pass
+#2 <head>
+class Head(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "head"
+        self.border = data
+    pass
+#4 <body>
+class Body(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "body"
+        self.border = data
+    pass
+#13 <center>
+class Center(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "center"
+        self.border = data
+    pass
+class Strong(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "strong"
+        self.border = data
+    pass
+class Em(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "em"
+        self.border = data
+    pass
+class Ol(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "ol"
+        self.border = data
+    pass
+class Ul(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "ul"
+        self.border = data
+    pass
+class Li(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "li"
+        self.border = data
+    pass
+#5 <div>
+class Div(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "div"
+        self.border = data
+    pass
+#6 <header>
+class Header(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "header"
+        self.border = data
+    pass
+#7 <footer>
+class Footer(Tag):
+    def __init__(self, data):
+        Tag.__init__(self)
+        self.name = "footer"
+        self.border = data
+    pass
+#экземпляры класса использующие только атрибут текст
+#8 <h1,2,3>
+class H1(Tag):
+    def __init__(self,text, href, typ):
+        Tag.__init__(self)
+        self.name = "h1"
+        self.content = text
+        self.href = href
+        self.type = typ
+    pass
+class H2(Tag):
+    def __init__(self,text):
+        Tag.__init__(self)
+        self.name = "h2"
+        self.content = text
+    pass
+class H3(Tag):
+    def __init__(self,text):
+        Tag.__init__(self)
+        self.name = "h3"
+        self.content = text
+    pass
+class P(Tag):
+    def __init__(self,text, href):
+        Tag.__init__(self)
+        self.name = "p"
+        self.content = text
+    pass
+#3 <title>
+class Title(Tag):
+    def __init__(self,text):
+        Tag.__init__(self)
+        self.name = "title"
+        self.content = text
+    pass
+#экземпляры класса использующие только атрибут имя
+#12 <br/>
+class Br(Tag):
+    def __init__(self):
+        Tag.__init__(self)
+        self.name = "br"
+#13 <hr/>
+class Hr(Tag):
+    def __init__(self):
+        Tag.__init__(self)
+        self.name = "hr"
+
+#экземпляры класса использующие много чего
+#9 <link>
+
+#10 <a>
+class A(Tag):
+    def __init__(self,text, href):
+        Tag.__init__(self)
+        self.name = "a"
+        self.content = text
+        self.href = href
+    pass
+
+#11 <style>
+class Style(Tag):
+    def __init__(self, text, href):
+        Tag.__init__(self)
+        self.name = "style"
+        self.content = text
+        self.href = href
+    pass
+class Img(Tag):
+    def __init__(self, href):
+        Tag.__init__(self)
+        self.name = "img"
+        self.href = href
+    pass
+
+#создание самой страницы.
+ht = Html([
+           Head([
+                   Title("Python"),
+                  Style("center { text-align:left; }", "type='text/css'")
+                   ]),
+             Body([
+                   Div([
+                        Header([
+                            H1("Python", "style='font-size:45px'", "align='center'")                          
+                        ]),
+                        Img("src='https://top10a.ru/wp-content/uploads/2019/06/3-2.png'"),
+                        Center([
+                                H2("Разделы"),
+                                Hr(),
+                                Div([
+                                    Ol([
+                                        Li([
+                                            Strong([
+                                                A("Базовый синтаксис Python","href='http://pythonicway.com/python-basic-syntax'")
+                                                ])
+                                            ]),
+                                        Li([
+                                            Strong([
+                                                A("Условные операторы в Python","href='https://devpractice.ru/python-lesson-5-if-while-for-operators/'")
+                                                ])
+                                            ]),
+                                        Li([
+                                            Strong([
+                                                A("Функции в Python","href='https://pythonworld.ru/tipy-dannyx-v-python/vse-o-funkciyax-i-ix-argumentax.html'")
+                                                ])
+                                            ]),
+                                        Li([
+                                            Strong([
+                                                A("Объектно-ориентированное программирование в Python","href='https://python-scripts.com/object-oriented-programming-in-python'")
+                                                ])
+                                            ]),
+                                        Ul([
+                                            Em([
+                                                 Li([
+                                                A("Классы и объекты","href='https://devpractice.ru/python-lesson-14-classes-and-objects/'")  
+                                            ]),
+                                                 Li([
+                                                A("Методы","href='https://pythonworld.ru/tipy-dannyx-v-python/stroki-funkcii-i-metody-strok.html'")  
+                                            ]),
+                                                 Li([
+                                                A("Наследование,полиморфизм и инкапсуляция","href='http://pythonicway.com/education/python-oop-themes/21-python-inheritance'")  
+                                            ])
+                                                ])
+                                            ])
+
+                                    
+                                     ])
+                                   ])
+                                ])
+                        ]),
+                   Footer([
+                           H3("Просмотреть код данной страницы можно по следующей ссылке:"),
+                           A("Репозиторий github","href='https://github.com/sophiabelkova/test_polytech.git'"),
+                           Br(),
+                           H3("Работу подготовила:"),
+                           A("Белькова София Вадимовна, студентка 1 курса группы 3431101/00001","href='https://vk.com/sofia_belkova'"),
+                           ])
+                    ])
+              ])
+print(ht.Generate())
+ht.GenerateFile("file.html")
+                                     
+                        
+            
+             
